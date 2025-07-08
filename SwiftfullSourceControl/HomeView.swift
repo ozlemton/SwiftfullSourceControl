4ed39973e98bdb506ec368ed3b8a64fd87abe375
ozlem <ozlemtonkal@gmail.com>
2025-07-08 16:30:56 +0300

    Home view(progress)

--- SwiftfullSourceControl/HomeView.swift
+++ SwiftfullSourceControl/HomeView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Text("Hello, World!")
+            Text("Hello, World!")
+            Text("Hello, World!")
+
+        }
     }
 }
 
