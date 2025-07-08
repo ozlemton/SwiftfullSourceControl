cd1152ed06416477a5025cc5034fb00eeb65bed3
ozlem <ozlemtonkal@gmail.com>
2025-07-08 16:29:03 +0300

    Add Home View

---
+++ SwiftfullSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfullSourceControl
+//
+//  Created by özlem tonkal on 8.07.2025.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
